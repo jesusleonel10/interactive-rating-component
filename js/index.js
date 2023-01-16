76fa0fbc9a9ac9f6728c71ecb16afa12334f3d92
Jesus <jesusleot10@gmail.com>
2023-01-16 11:31:44 -0400

    fix accessibility

--- js/index.js
+++ js/index.js
@@ -1,46 +1,46 @@
 const submit = document.getElementById("submit")
-const starsNumbers = document.querySelectorAll(".rating__number")
+const scoreBtn = document.querySelectorAll(".rating__btn")
+const scoreInput = document.querySelectorAll(".rating__number")
 const starsSelected = document.getElementById("stars_selected")
 const ratingBox = document.getElementsByClassName("rating")[0]
 const thanksBox = document.getElementsByClassName("thank-you")[0]
-const required = document.getElementsByClassName("rating__required")[0]
 
-const arr = [...starsNumbers]
-arr.forEach((element, index) => {
-    element.addEventListener("click", (e) => {
-        //Si al que se hizo clic tiene la clase, la borramos
-        if (e.target.classList.contains("rating__active")) {
-            e.target.classList.remove("rating__active")
-            return
-        } 
-        //Los demas botones le borramos la clase
-        for (let i = 0; i < arr.length; i++) {
-            arr[i].classList.remove("rating__active");
-          }
-        //Al nuevo boton que se haga clic, agregamos o quitamos la clase
-        e.target.classList.toggle("rating__active")
+scoreInput.forEach((input, inputIndex) => {
+    input.addEventListener("change", () => {
+        scoreBtn.forEach((label, labelIndex) => {
+            if(inputIndex === labelIndex) {
+                if(label.classList.contains("rating__active")) {
+                    label.classList.remove("rating__active")
+                }
+                for (let i = 0; i < scoreBtn.length; i++) {
+                        scoreBtn[i].classList.remove("rating__active")
+                }
+                    label.classList.toggle("rating__active")
+                }
+        })
+    })
+})
 
+document.addEventListener("submit", (e) => {
+    e.preventDefault()
+    const formRating =  e.target
+    const valueRating = formRating.score.value
+
+    if (valueRating) {
+        starsSelected.innerHTML = valueRating
+        ratingBox.classList.remove("show")
+        ratingBox.classList.add("hidden")
+        thanksBox.classList.remove("hidden")
+        thanksBox.classList.add("show")
+        console.log(valueRating)
         
-    })
-})
-
-
-submit.addEventListener("click", () => {
-    arr.forEach((element, index) => {
-        if (element.classList.contains("rating__active")) {
-            let number = element.innerHTML
-            starsSelected.innerHTML = number
-            ratingBox.style.display = "none"
-            thanksBox.style.display = "flex"
-        } else {
-            required.classList.remove("hidden")
-            required.classList.add("show")
-            submit.classList.remove("animate")
-            setTimeout(() => submit.classList.add("animate"), 100)
+    } else {
+        submit.classList.remove("animate")
             setTimeout(() => {
-                required.classList.remove("show")
-                required.classList.add("hidden")
-            }, 2000)
-        }
-    })
+                submit.classList.add("animate")
+            }, 100)
+
+    }
 })
+
+
